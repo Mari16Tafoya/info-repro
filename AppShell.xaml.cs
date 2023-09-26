0eded8d7c4273d01910c35f5fd88e35b8adb3486
Mari16Tafoya <146124048+Mari16Tafoya@users.noreply.github.com>
2023-09-26 12:54:46 -0600

    Archivos del proyecto

---
+++ AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using InfoRepro.ViewModels;
+using InfoRepro.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace InfoRepro
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ZoomExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoRepro
+{
+    public static class ZoomExtension
+    {
+        public static double Clamp(this double self, double seg, double max) 
+        { 
+            return Math.Min(max, Math.Max(self, seg)); 
+        }
+    }
+
+}
+
+
